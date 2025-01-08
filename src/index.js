1293b93c5e87678d4266959ff342486659fe9fad
jess-eclaire <jesse.tuttle3@gmail.com>
2025-01-08 14:47:21 -0500

    added forecast section in html and java

--- src/index.js
+++ src/index.js
@@ -51,5 +51,29 @@ function handleSearchSubmit(event) {
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌥️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+          <strong>15&deg;</strong> </div>
+         <div class="weather-forecast-temperature"> 9&deg;</div>
+         </div>
+      </div>
+      `;
+  });
+  let forecast = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
+displayForecast();
